76c70733744165eda1472de24a36fadbb3730aa0
foolish314159 <foolish1337@gmx.de>
2017-07-20 22:01:33 +0200

    Attempt to fix WebView offline 2

--- app/src/main/java/com/github/foolish314159/nhkeasynews/ui/NHKArticleFragment.kt
+++ app/src/main/java/com/github/foolish314159/nhkeasynews/ui/NHKArticleFragment.kt
@@ -117,7 +117,7 @@ class NHKArticleFragment : Fragment() {
     private fun loadArticle(text: String) {
         if (!loaded) {
             this.articleWebView?.settings?.javaScriptEnabled = true
-            // TODO: why does this not work when offline?
+            // TODO: why does this not work when offline? a
             //this.articleWebView?.loadData(URLEncoder.encode(text).replace("\\+", " "), "text/html", "utf-8")
             this.articleWebView?.loadDataWithBaseURL(null, text, "text/html", "utf-8", null)
             loaded = true
